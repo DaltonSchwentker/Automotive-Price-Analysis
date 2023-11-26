d420ae77e52bb8150d3c9cc73307565de4bc968c
Dalton Schwentker <d_schwentker@yahoo.com>
2023-11-26 09:30:13 -0600

    test/prod switch

--- batch_vin_decode_clean.py
+++ batch_vin_decode_clean.py
@@ -1,204 +1,197 @@
+import os
+import requests
 from requests_futures.sessions import FuturesSession
 from tqdm import tqdm
-import requests
 import json
 import pandas as pd
 import re
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
-# Database connection parameters
-dbname = 'vehicle_data_db'
-user = 'postgres'
-password = 'admin'
-host = 'localhost'
+load_dotenv()
 
-# Create a SQLAlchemy engine for Pandas
-engine = create_engine(f'postgresql://{user}:{password}@{host}/{dbname}')
-
-# Read data from PostgreSQL table
-df = pd.read_sql('SELECT * FROM vehicle_data', engine)
-
-url = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/'
-
-chunk_size = 50  # Set chunk size to 50
-session = FuturesSession(max_workers=15)  # Set the number of concurrent requests
-
-futures = []
-
-for i in range(0, len(df), chunk_size):
-    chunk = df[i:i+chunk_size]
-    vins = ';'.join(chunk['VIN'])
-    post_fields = {'format': 'json', 'data': vins}
-    futures.append(session.post(url, data=post_fields))
-
-results = []
-
-for future in tqdm(futures, total=len(futures)):
-    response = future.result()
+mode = os.getenv('MODE')
+print(mode)
     
-    # Check the response status code
-    if response.status_code != 200:
-        print(f"Received unexpected status code {response.status_code}: {response.text}")
-        continue
+def create_db_engine():
+    dbname = os.getenv('PROD_DB_NAME')
+    user = os.getenv('PROD_DB_USER')
+    password = os.getenv('PROD_DB_PASS')
+    host = os.getenv('PROD_DB_HOST')
+    return create_engine(f'postgresql://{user}:{password}@{host}/{dbname}')
 
-    try:
-        data = json.loads(response.text)
-        for result in data['Results']:
-            if 'Message' in result:
-                print(f"Message from API: {result['Message']}")
-            else:
-                results.append({
-                    'VIN': result['VIN'],
-                    'Make': result['Make'],
-                    'Model': result['Model'],
-                    'Year': result['ModelYear'],
-                    'Trim': result['Trim']
-                })
-    except json.JSONDecodeError:
-        print(f"Failed to decode JSON from response: {response.text}")
+# Function to fetch VIN details
+def fetch_vin_details(df, chunk_size=50):
+    url = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/'
+    session = FuturesSession(max_workers=15)
+    futures = []
+    results = []
 
-results_df = pd.DataFrame(results)
-df = pd.merge(df, results_df, on='VIN')
+    for i in range(0, len(df), chunk_size):
+        chunk = df[i:i+chunk_size]
+        vins = ';'.join(chunk['VIN'])
+        post_fields = {'format': 'json', 'data': vins}
+        futures.append(session.post(url, data=post_fields))
 
-# Cleaning "Car Price" column
-# Remove any non-digit characters, including currency symbols
-df['CarPrice'] = df['CarPrice'].replace('[^\d.]+', '', regex=True)
-# Convert to numeric, replacing any non-convertible values with NaN
-df['CarPrice'] = pd.to_numeric(df['CarPrice'], errors='coerce')
+    for future in tqdm(futures, total=len(futures)):
+        response = future.result()
+        if response.status_code != 200:
+            print(f"Received unexpected status code {response.status_code}: {response.text}")
+            continue
 
-# Cleaning "Car Mileage" column
-df['CarMileage'] = df['CarMileage'].str.replace(',', '').str.replace(' mi\.', '', regex=True)
-df['CarMileage'] = df['CarMileage'].replace('–', pd.NA)
-df['CarMileage'] = pd.to_numeric(df['CarMileage'], errors='coerce')
+        try:
+            data = json.loads(response.text)
+            results += parse_vin_response(data)
+        except json.JSONDecodeError:
+            print(f"Failed to decode JSON from response: {response.text}")
 
+    return pd.DataFrame(results)
 
-# Define general color buckets with associated keywords
-color_buckets = {
-    'Red': ['Red', 'Rosso', 'Crimson', 'Ruby'],
-    'Black': ['Black', 'Noir', 'Ebony'],
-    'White': ['White', 'Ivory'],
-    'Gray': ['Gray', 'Grey', 'Graphite', 'Metallic'],
-    'Blue': ['Blue', 'Azure', 'Cobalt'],
-    'Green': ['Green', 'Emerald'],
-    'Brown': ['Brown', 'Chocolate', 'Sandstone'],
-    'Yellow': ['Yellow', 'Gold'],
-    'Orange': ['Orange'],
-}
+# Function to parse the VIN response
+def parse_vin_response(data):
+    results = []
+    for result in data['Results']:
+        if 'Message' in result:
+            print(f"Message from API: {result['Message']}")
+        else:
+            results.append({
+                'VIN': result['VIN'],
+                'Make': result['Make'],
+                'Model': result['Model'],
+                'Year': result['ModelYear'],
+                'Trim': result['Trim']
+            })
+    return results
 
-# Define a function to map specific colors to general color buckets
-def map_to_general_color(specific_color):
-    for general_color, keywords in color_buckets.items():
-        if any(keyword.lower() in specific_color.lower() for keyword in keywords):
-            return general_color
-    return 'Other'
+# Function for cleaning and mapping data
+def clean_and_map_data(df):
+    # Cleaning "Car Price" column
+    df['CarPrice'] = df['CarPrice'].replace('[^\d.]+', '', regex=True)
+    df['CarPrice'] = pd.to_numeric(df['CarPrice'], errors='coerce')
 
-# Apply the function to the 'Exterior Color' column
-df['ExteriorColorGeneral'] = df['ExteriorColor'].apply(map_to_general_color)
+    # Cleaning "Car Mileage" column
+    df['CarMileage'] = df['CarMileage'].str.replace(',', '').str.replace(' mi\.', '', regex=True)
+    df['CarMileage'] = df['CarMileage'].replace('–', pd.NA)
+    df['CarMileage'] = pd.to_numeric(df['CarMileage'], errors='coerce')
 
-# Apply the function to the 'Interior Color' column
-df['InteriorColorGeneral'] = df['InteriorColor'].apply(map_to_general_color)
+    # Color mapping
+    color_buckets = {
+        'Red': ['Red', 'Rosso', 'Crimson', 'Ruby'],
+        'Black': ['Black', 'Noir', 'Ebony'],
+        'White': ['White', 'Ivory'],
+        'Gray': ['Gray', 'Grey', 'Graphite', 'Metallic'],
+        'Blue': ['Blue', 'Azure', 'Cobalt'],
+        'Green': ['Green', 'Emerald'],
+        'Brown': ['Brown', 'Chocolate', 'Sandstone'],
+        'Yellow': ['Yellow', 'Gold'],
+        'Orange': ['Orange'],
+    }
 
-# Define a mapping for drivetrain categories
-drivetrain_mapping = {
-    'Front-wheel Drive': 'FWD',
-    'All-wheel Drive': 'AWD',
-    'Four-wheel Drive': '4WD',
-    'Rear-wheel Drive': 'RWD',
-    'FWD': 'FWD',
-    'AWD': 'AWD',
-    '4WD': '4WD',
-    'RWD': 'RWD',
-    '–': 'Other'
-}
-
-# Apply the mapping to the 'Drivetrain' column
-df['DrivetrainGeneral'] = df['Drivetrain'].map(drivetrain_mapping)
-
-# Define a mapping for fuel type categories
-def map_fuel_type(fuel_type):
-    if fuel_type in ['Gasoline', '–', '']:
-        return 'Gasoline'
-    elif fuel_type == 'Diesel':
-        return 'Diesel'
-    elif fuel_type == 'Electric':
-        return 'Electric'
-    elif fuel_type == 'E85 Flex Fuel':
-        return 'Flex Fuel'
-    elif fuel_type == 'Hybrid':
-        return 'Hybrid'
-    else:
+    def map_to_general_color(specific_color):
+        for general_color, keywords in color_buckets.items():
+            if any(keyword.lower() in specific_color.lower() for keyword in keywords):
+                return general_color
         return 'Other'
 
-# Apply the mapping to the 'Fuel Type' column
-df['FuelTypeGeneral'] = df['FuelType'].apply(map_fuel_type)
+    df['ExteriorColorGeneral'] = df['ExteriorColor'].apply(map_to_general_color)
+    df['InteriorColorGeneral'] = df['InteriorColor'].apply(map_to_general_color)
 
-# Define a function to map specific transmissions to general transmission categories
-def map_transmission(transmission):
-    if 'automatic' in transmission.lower() or 'cvt' in transmission.lower() or 'tiptronic' in transmission.lower() or 'shiftronic' in transmission.lower():
-        return 'Automatic'
-    elif 'manual' in transmission.lower() or 'm/t' in transmission.lower():
-        return 'Manual'
-    else:
-        return 'Other'
+    # Drivetrain mapping
+    drivetrain_mapping = {
+        'Front-wheel Drive': 'FWD',
+        'All-wheel Drive': 'AWD',
+        'Four-wheel Drive': '4WD',
+        'Rear-wheel Drive': 'RWD',
+        'FWD': 'FWD',
+        'AWD': 'AWD',
+        '4WD': '4WD',
+        'RWD': 'RWD',
+        '–': 'Other'
+    }
 
-# Apply the function to the 'Transmission' column
-df['TransmissionGeneral'] = df['Transmission'].apply(map_transmission)
+    df['DrivetrainGeneral'] = df['Drivetrain'].map(drivetrain_mapping)
 
-# Define a function to extract the engine size
-def extract_engine_size(engine):
-    engine_size = None
-    # Extract engine size using regular expression (e.g., "2.0L")
-    match = re.search(r'\d+\.\d+L', engine)
-    if match:
-        engine_size = match.group(0)
-    return engine_size
+    # Fuel type mapping
+    def map_fuel_type(fuel_type):
+        if fuel_type in ['Gasoline', '–', '']:
+            return 'Gasoline'
+        elif fuel_type == 'Diesel':
+            return 'Diesel'
+        elif fuel_type == 'Electric':
+            return 'Electric'
+        elif fuel_type == 'E85 Flex Fuel':
+            return 'Flex Fuel'
+        elif fuel_type == 'Hybrid':
+            return 'Hybrid'
+        else:
+            return 'Other'
 
-# Apply the function to the 'Engine' column
-df['EngineSize'] = df['Engine'].apply(extract_engine_size)
+    df['FuelTypeGeneral'] = df['FuelType'].apply(map_fuel_type)
 
-# Define a function to extract the engine configuration
-def extract_engine_configuration(engine):
-    # Extract engine configuration using regular expression (e.g., "I4", "V6")
-    match = re.search(r'[IViv]\d+', engine)
-    if match:
-        return match.group(0)
-    return None
+    # Transmission mapping
+    def map_transmission(transmission):
+        if 'automatic' in transmission.lower() or 'cvt' in transmission.lower() or 'tiptronic' in transmission.lower() or 'shiftronic' in transmission.lower():
+            return 'Automatic'
+        elif 'manual' in transmission.lower() or 'm/t' in transmission.lower():
+            return 'Manual'
+        else:
+            return 'Other'
 
-# Apply the function to the 'Engine' column
-df['EngineConfiguration'] = df['Engine'].apply(extract_engine_configuration)
+    df['TransmissionGeneral'] = df['Transmission'].apply(map_transmission)
 
-# Define a function to extract the fuel system
-def extract_fuel_system(engine):
-    # Common fuel system types
-    fuel_systems = ['GDI', 'MPFI', 'DI', 'SFI']
-    for fs in fuel_systems:
-        if fs in engine:
-            return fs
-    return None
+    # Engine size and configuration extraction
+    def extract_engine_size(engine):
+        match = re.search(r'\d+\.\d+L', engine)
+        return match.group(0) if match else None
 
-# Apply the function to the 'Engine' column
-df['Fuel System'] = df['Engine'].apply(extract_fuel_system)
+    def extract_engine_configuration(engine):
+        match = re.search(r'[IViv]\d+', engine)
+        return match.group(0) if match else None
 
-# Define a function to extract the turbocharged indicator
-def extract_turbocharged(engine):
-    return int('turbo' in engine.lower())
+    df['EngineSize'] = df['Engine'].apply(extract_engine_size)
+    df['EngineConfiguration'] = df['Engine'].apply(extract_engine_configuration)
 
-# Define a function to extract the hybrid indicator
-def extract_hybrid(engine):
-    return int('hybrid' in engine.lower())
+    # Extract the fuel system
+    def extract_fuel_system(engine):
+        fuel_systems = ['GDI', 'MPFI', 'DI', 'SFI']
+        for fs in fuel_systems:
+            if fs in engine:
+                return fs
+        return None
 
-# Apply the functions to the 'Engine' column
-df['Turbocharged'] = df['Engine'].apply(extract_turbocharged)
-df['Hybrid'] = df['Engine'].apply(extract_hybrid)
+    df['Fuel System'] = df['Engine'].apply(extract_fuel_system)
 
-# Drop the specified columns
-df.drop(columns=['CarName', 'ExteriorColor', 'InteriorColor', 'Drivetrain', 'FuelType', 'Transmission', 'Engine'], inplace=True)
+    # Extract turbocharged and hybrid indicators
+    df['Turbocharged'] = df['Engine'].apply(lambda e: int('turbo' in e.lower()))
+    df['Hybrid'] = df['Engine'].apply(lambda e: int('hybrid' in e.lower()))
 
-existing_vins = pd.read_sql('SELECT "VIN", "TimeStamp" FROM cleaned_vehicle_data', engine)  # Note the lowercase 'vin'
-df = df.merge(existing_vins, on=['VIN', 'TimeStamp'], how='left', indicator=True)  # Note the lowercase 'vin'
-df = df[df['_merge'] == 'left_only'].drop(columns=['_merge'])
+    return df
 
-# Append non-duplicate cleaned data into the new table
-df.to_sql('cleaned_vehicle_data', engine, if_exists='append', index=False)
+# Main function
+def main():
 
-print("Data cleaning and loading complete.")
+    engine = create_db_engine()
+
+    # Define table names based on mode
+    data_table = 'vehicle_data_test_env' if mode == 'test' else 'vehicle_data'
+    cleaned_data_table = 'cleaned_vehicle_data_test_env' if mode == 'test' else 'cleaned_vehicle_data'
+
+    # Fetch and clean data
+    df = pd.read_sql(f'SELECT * FROM {data_table}', engine)
+    vin_details_df = fetch_vin_details(df)
+    df = pd.merge(df, vin_details_df, on='VIN')
+    df = clean_and_map_data(df)
+
+    # Drop the specified columns
+    df.drop(columns=['CarName', 'ExteriorColor', 'InteriorColor', 'Drivetrain', 'FuelType', 'Transmission', 'Engine'], inplace=True)
+
+    # Check for duplicates and append non-duplicates to the new table
+    existing_vins = pd.read_sql(f'SELECT "VIN", "TimeStamp" FROM {cleaned_data_table}', engine)
+    df = df.merge(existing_vins, on=['VIN', 'TimeStamp'], how='left', indicator=True)
+    df = df[df['_merge'] == 'left_only'].drop(columns=['_merge'])
+    df.to_sql(cleaned_data_table, engine, if_exists='append', index=False)
+
+    print(f"Data cleaning and loading complete loaded to: {cleaned_data_table} used data from {data_table}.")
+
+if __name__ == '__main__':
+    main()
--- updated_cars_com_scraper.py
+++ updated_cars_com_scraper.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
@@ -5,77 +6,120 @@ from bs4 import BeautifulSoup
 import time
 import random
 from datetime import datetime
-from fake_useragent import UserAgent
 import logging
 import psycopg2
 from psycopg2 import sql
 from uszipcode import SearchEngine
+from fake_useragent import UserAgent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mode = os.getenv('MODE')
+print(mode)
 
 # Initialize search engine
 search = SearchEngine()
 
-# Get list of all zipcodes (limited number for performance)
-all_zipcodes = search.by_city_and_state(city=None, state=None, returns=500)  # Adjust returns as needed
+# Load environment variables for database credentials
+DB_NAME = os.getenv('PROD_DB_NAME')
+DB_USER = os.getenv('PROD_DB_USER')
+DB_PASS = os.getenv('PROD_DB_PASS')
+DB_HOST = os.getenv('PROD_DB_HOST')
 
-# Extract just the ZIP code strings
-zip_code_list = [z.zipcode for z in all_zipcodes]
+print(DB_NAME)
+print(DB_USER)
+print(DB_PASS)
+print(DB_HOST)
 
-# Randomly select a ZIP code
-selected_zip = random.choice(zip_code_list)
+
+# Define table names based on mode
+data_table = 'vehicle_data_test_env' if mode == 'test' else 'vehicle_data'
+
+print(f'Writing to: {data_table})
+
 
 # Initialize detailed logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Define pages to scrape
-pages_to_scrape = 20
-
+PAGES_TO_SCRAPE = 2
 
 ua = UserAgent()
 
 # Setup retry strategy
 retry_strategy = Retry(
-    total=3,  # Total number of retries to allow
-    status_forcelist=[429, 500, 502, 503, 504],  # Status codes to retry for
-    method_whitelist=["HEAD", "GET", "OPTIONS"],  # HTTP methods to retry
-    backoff_factor=1  # Delay factor between retry attempts
+    total=3,
+    status_forcelist=[429, 500, 502, 503, 504],
+    method_whitelist=["HEAD", "GET", "OPTIONS"],
+    backoff_factor=1
 )
-
 
 adapter = HTTPAdapter(max_retries=retry_strategy)
 http = requests.Session()
 http.mount("https://", adapter)
 http.mount("http://", adapter)
 
+def get_random_zip_code():
+    # Efficiently fetch a random ZIP code from the database or an external source
+    # Placeholder for your implementation
+    return random.choice(search.by_city_and_state(city=None, state=None, returns=500)).zipcode
+
 def insert_into_database(data):
+    with psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST) as connection:
+        with connection.cursor() as cursor:
+            insert_query = sql.SQL(f"""
+                INSERT INTO public.{data_table} (
+                    "CarName", "CarPrice", "CarMileage", "ExteriorColor", 
+                    "InteriorColor", "Drivetrain", "FuelType", "Transmission", 
+                    "Engine", "VIN", "TimeStamp", "Source", "ZipLocation"
+                ) VALUES (
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+                )
+            """)
+            cursor.executemany(insert_query, data)
+            connection.commit()
+
+def fetch_car_details(car_url, headers):
+    # Fetch and parse car details from the car detail page
     try:
-        connection = psycopg2.connect(
-            dbname='vehicle_data_db',
-            user='postgres',
-            password='admin',
-            host='localhost'
-        )
-        cursor = connection.cursor()
+        car_response = http.get(car_url, headers=headers, timeout=10)
+        car_response.raise_for_status()
+        car_soup = BeautifulSoup(car_response.content, 'html.parser')
+        return car_soup
+    except requests.RequestException as e:
+        logging.error(f"Error fetching car details: {e}")
+        return None
 
-        insert_query = sql.SQL("""
-            INSERT INTO public.vehicle_data (
-                "CarName", "CarPrice", "CarMileage", "ExteriorColor", 
-                "InteriorColor", "Drivetrain", "FuelType", "Transmission", 
-                "Engine", "VIN", "TimeStamp", "Source", "ZipLocation"
-            ) VALUES (
-                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-            )
-        """)
+def process_car_listing(car_listing, headers, selected_zip):
+    try:
+        car_name = car_listing.find('h2', class_='title').text.strip()
+        car_price = car_listing.find('span', class_='primary-price').text.strip()
+        car_url = "https://www.cars.com" + car_listing.find('a')['href']
 
-        cursor.executemany(insert_query, data)
-        connection.commit()
+        car_soup = fetch_car_details(car_url, headers)
+        if not car_soup:
+            return None
+
+        car_specs = car_soup.find('dl', class_='fancy-description-list')
+        specs_dict = {term.text.strip(): desc.text.strip() for term, desc in zip(car_specs.find_all('dt'), car_specs.find_all('dd')) if term.text.strip() in ['Exterior color', 'Interior color', 'Drivetrain', 'Fuel type', 'Transmission', 'Engine', 'VIN', 'Mileage']}
+
+        timestamp = datetime.now()
+        scrape_source = "Cars.com"
+        zip_location = selected_zip
+
+        return [
+            car_name, car_price, specs_dict.get('Mileage'), specs_dict.get('Exterior color'), 
+            specs_dict.get('Interior color'), specs_dict.get('Drivetrain'), specs_dict.get('Fuel type'), 
+            specs_dict.get('Transmission'), specs_dict.get('Engine'), specs_dict.get('VIN'), 
+            timestamp, scrape_source, zip_location
+        ]
     except Exception as e:
-        logging.error(f"An error occurred while inserting data into database: {e}")
-    finally:
-        if connection is not None:
-            connection.close()
+        logging.error(f"Error processing car listing: {e}")
+        return None
 
-def scrape_car_data(page_number):
-    url = f"https://www.cars.com/shopping/results/?dealer_id=&keyword=&list_price_max=&list_price_min=&makes[]=&maximum_distance=50&mileage_max=&monthly_payment=&page={page_number}&page_size=20&sort=distance&stock_type=used&year_max=&year_min=&zip={selected_zip}"
+def scrape_car_data(page_number, selected_zip):
+    url = f"https://www.cars.com/shopping/results/?page={page_number}&zip={selected_zip}"
     headers = {'User-Agent': ua.random}
 
     try:
@@ -88,43 +132,28 @@ def scrape_car_data(page_number):
     soup = BeautifulSoup(response.content, 'html.parser')
     car_data = []
     for car_listing in soup.find_all('div', class_='vehicle-card-main js-gallery-click-card'):
-        try:
-            car_name = car_listing.find('h2', class_='title').text.strip()
-            car_price = car_listing.find('span', class_='primary-price').text.strip()
-            car_url = "https://www.cars.com" + car_listing.find('a')['href']
-
-            car_response = requests.get(car_url, headers=headers, timeout=10)
-            car_soup = BeautifulSoup(car_response.content, 'html.parser')
-
-            car_specs = car_soup.find('dl', class_='fancy-description-list')
-            specs_dict = {term.text.strip(): desc.text.strip() for term, desc in zip(car_specs.find_all('dt'), car_specs.find_all('dd')) if term.text.strip() in ['Exterior color', 'Interior color', 'Drivetrain', 'Fuel type', 'Transmission', 'Engine', 'VIN', 'Mileage']}
-
-            timestamp = datetime.now()
-            scrape_source = "Cars.com"
-            zip_location = selected_zip
-
-            car_data.append([
-                car_name, car_price, specs_dict.get('Mileage'), specs_dict.get('Exterior color'), 
-                specs_dict.get('Interior color'), specs_dict.get('Drivetrain'), specs_dict.get('Fuel type'), 
-                specs_dict.get('Transmission'), specs_dict.get('Engine'), specs_dict.get('VIN'), 
-                timestamp, scrape_source, zip_location
-            ])
-
-            logging.debug(f"Processed car listing: {car_name}")
-            time.sleep(random.uniform(1, 3))  # Random delay
-        except Exception as e:
-            logging.error(f"Error processing a car listing: {e}")
-            continue
+        processed_data = process_car_listing(car_listing, headers, selected_zip)
+        if processed_data:
+            car_data.append(processed_data)
+        time.sleep(random.uniform(1, 3))  # Random delay
 
     return car_data
 
-all_car_data = []
-for page_number in range(1, pages_to_scrape + 1):
-    logging.info(f"Scraping page {page_number}...")
-    car_data = scrape_car_data(page_number)
-    if car_data:
-        insert_into_database(car_data)
-        logging.info(f"Page {page_number} data inserted into database")
-    time.sleep(random.uniform(1, 7))
+def main():
+    selected_zip = get_random_zip_code()
+    all_car_data = []
+    for page_number in range(1, PAGES_TO_SCRAPE + 1):
+        logging.info(f"Scraping page {page_number}...")
+        car_data = scrape_car_data(page_number, selected_zip)
+        if car_data:
+            all_car_data.extend(car_data)
 
-logging.info(f"{pages_to_scrape} Pages scraped and data inserted into database successfully.")
+    # Batch insert data into database
+    if all_car_data:
+        insert_into_database(all_car_data)
+        logging.info(f"All data inserted into database")
+
+    logging.info(f"{PAGES_TO_SCRAPE} Pages scraped and data inserted into database successfully.")
+
+if __name__ == "__main__":
+    main()
