be44829de78b5431b632784e0a5baac67c434b0f
Dalton Schwentker <d_schwentker@yahoo.com>
2023-11-30 18:07:55 -0600

    scrape model level detail

--- scrape_maintenance_data.py
+++ scrape_maintenance_data.py
@@ -16,38 +16,58 @@ DB_PASS = os.getenv('PROD_DB_PASS')
 DB_HOST = os.getenv('PROD_DB_HOST')
 
 # Define the top 15 car brands
-brands = ["toyota", "ford", "chevrolet", "honda", "nissan", "hyundai", "subaru", "kia", "mercedes-benz", "bmw", "volkswagen", "audi", "mazda", "dodge", "lexus"]
+brands = ["toyota", "ford", "chevrolet", "honda", "nissan", "hyundai", "subaru", "kia", 
+          "mercedes-benz", "bmw", "volkswagen", "audi", "mazda", "dodge", "lexus"]
 
 # Initialize User Agent and logging
 ua = UserAgent()
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def get_maintenance_data(brand):
-    url = f"https://caredge.com/{brand}/maintenance"
+def get_model_urls(brand):
+    brand_url = f"https://caredge.com/{brand}/maintenance"
     headers = {'User-Agent': ua.random}
+    model_urls = []
+
+    try:
+        response = requests.get(brand_url, headers=headers)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        for row in soup.find_all("tr"):
+            links = row.find_all("a")
+            for link in links:
+                if 'maintenance' in link.get('href'):
+                    model_urls.append(link.get('href'))
+    except requests.RequestException as e:
+        logging.error(f"Request to {brand_url} failed: {e}")
+    
+    return model_urls
+
+def get_maintenance_data(url_suffix):
+    url = f"https://caredge.com{url_suffix}"
+    headers = {'User-Agent': ua.random}
+    data = []
 
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+        table = soup.find("table", {"class": "table table-striped table-bordered table-hover"})
+        
+        if not table:
+            logging.error(f"No data table found for {url_suffix}")
+            return []
+
+        for row in table.find_all("tr")[1:]:
+            cols = row.find_all("td")
+            year = cols[0].text.strip()
+            major_repair_prob = cols[1].text.strip()
+            annual_costs = cols[2].text.strip()
+            data.append((url_suffix.split('/')[1], url_suffix.split('/')[2], year, major_repair_prob, annual_costs))
+
     except requests.RequestException as e:
         logging.error(f"Request to {url} failed: {e}")
-        return []
-
-    soup = BeautifulSoup(response.content, 'html.parser')
-    table = soup.find("table", {"class": "table table-striped table-bordered table-hover"})
     
-    if not table:
-        logging.error(f"No data table found for {brand}")
-        return []
-
-    data = []
-    for row in table.find_all("tr")[1:]:  # Skip header row
-        cols = row.find_all("td")
-        year = cols[0].text.strip()
-        major_repair_prob = cols[1].text.strip()
-        annual_costs = cols[2].text.strip()
-        data.append((brand, year, major_repair_prob, annual_costs))
-
     return data
 
 def insert_into_database(data):
@@ -55,9 +75,9 @@ def insert_into_database(data):
         with connection.cursor() as cursor:
             insert_query = sql.SQL("""
                 INSERT INTO public.car_maintenance_data (
-                    "Brand", "Year", "MajorRepairProbability", "AnnualCosts"
+                    "Brand", "Model", "Year", "MajorRepairProbability", "AnnualCosts"
                 ) VALUES (
-                    %s, %s, %s, %s
+                    %s, %s, %s, %s, %s
                 )
             """)
             cursor.executemany(insert_query, data)
@@ -66,10 +86,14 @@ def insert_into_database(data):
 def main():
     all_data = []
     for brand in brands:
-        logging.info(f"Fetching maintenance data for {brand}")
-        brand_data = get_maintenance_data(brand)
-        if brand_data:
-            all_data.extend(brand_data)
+        logging.info(f"Fetching model URLs for {brand}")
+        model_urls = get_model_urls(brand)
+        
+        for model_url in model_urls:
+            logging.info(f"Fetching maintenance data for {model_url}")
+            model_data = get_maintenance_data(model_url)
+            if model_data:
+                all_data.extend(model_data)
 
     if all_data:
         insert_into_database(all_data)
